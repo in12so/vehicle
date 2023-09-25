70d032db277f959bd3783330e8cd1818db3f1483
in12so <73948915+in12so@users.noreply.github.com>
2023-09-25 22:24:28 +0100

    constructors Dog

--- src/vehicleprogram/Vehicle.java
+++ src/vehicleprogram/Vehicle.java
@@ -11,10 +11,19 @@ public class Vehicle {
     String model;
     float economy;
     int distance = 0;
+    int noOfWheels;
+    
     
     //constructor
     
-    
+    Vehicle(){
+        
+        type = "Bike";
+        model = "Kawasaki KXF250F";
+        economy = 60.00F;
+        noOfWheels = 2;
+        
+    }
     
     //Methods
     public void drive(){
@@ -29,6 +38,7 @@ public class Vehicle {
         System.out.println("The make is "+type);
         System.out.println("The fuel economy is "+economy+ " mpg");
         System.out.println("The total distance travelled is "+distance);
+        System.out.println("The number of wheels is "+noOfWheels);
         System.out.println("========================================");
         
     }
