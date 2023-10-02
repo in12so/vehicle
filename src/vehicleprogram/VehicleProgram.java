47f0a8d7e3f3f1f7c7b77c5993aed0b3fda01c6a
in12so <73948915+in12so@users.noreply.github.com>
2023-10-02 22:20:59 +0100

    Add 3 new vehicle objects for car, van and bike

--- src/vehicleprogram/VehicleProgram.java
+++ src/vehicleprogram/VehicleProgram.java
@@ -9,37 +9,27 @@ public class VehicleProgram {
 
     public static void main(String[] args) {
      
-        Vehicle aCar = new Vehicle();
-        Vehicle aVan = new Vehicle();
-        Vehicle aNewVehicle = new Vehicle("Car","Renault", 55.00F,4,"John Doe",7);
+        Vehicle aCar = new Vehicle("Car","Renault", 55.00F,4,"John Doe",7);
+        Vehicle aVan = new Vehicle("Van","Volkswagen", 65.00F,4,"Paul Smith",3);
+        Vehicle aBike = new Vehicle("Bike","Ducati", 54.60F,2,"The Stig",5);
         
-        aCar.model = "Nissan";
-        aCar.type = "Qashqai";
-        aCar.economy = 55.40f;
-        
-        
-        aVan.model = "Volkswagen";
-        aVan.type = "Transporter";
-        aVan.economy = 40.00f;
-        
-//        System.out.println("The car is a "+aCar.model);
-//        System.out.println("The make is "+aCar.type);
-//        System.out.println("The fuel economy is "+aCar.economy+ " mpg");
+//        aCar.model = "Nissan";
+//        aCar.type = "Qashqai";
+//        aCar.economy = 55.40f;
 //        
-//        System.out.println("=======================================");
+//        
+//        aVan.model = "Volkswagen";
+//        aVan.type = "Transporter";
+//        aVan.economy = 40.00f;
         
-
-  
+ 
+     
         
-       
-       
+        aCar.drive();
         aCar.drive();
         aCar.displayDetails();
-        aCar.drive();
-        aCar.drive();
-        aCar.displayDetails();
         
-        aNewVehicle.displayDetails();
+        aBike.displayDetails();
         
         
         
